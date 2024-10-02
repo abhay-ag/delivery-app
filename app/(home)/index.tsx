e188728c9a3b2ad467727898bb39f9709d530ca2
Abhay Aggarwal <97821733+abhay-ag@users.noreply.github.com>
2024-10-02 16:00:26 +0530

    added notification button on the right hand side

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,19 +1,31 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import { View } from "react-native";
+import { Button, View } from "react-native";
 import DeliveryScreen from "./rideDetails";
 import UserProfileScreen from "./profile";
 import BloodDonationApp from "./notification";
 import HomeScreen from "./home";
 import QRCodeScanner from "./qr";
 import InProgressJobs from "./currentJobs";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Icon from "react-native-vector-icons/AntDesign";
 
 export default function Home() {
   const Drawer = createDrawerNavigator();
   return (
     <Drawer.Navigator
-      screenOptions={{
+      screenOptions={({ navigation }) => ({
         headerTitle: "",
-      }}
+        headerRight: () => (
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("Notifications");
+            }}
+            style={{ paddingHorizontal: 10 }}
+          >
+            <Icon name="bells" size={20} />
+          </TouchableOpacity>
+        ),
+      })}
       initialRouteName="home"
     >
       <Drawer.Screen name="Home" component={HomeScreen} />
